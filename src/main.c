2efb9bd3efb4aca22b5affe2a86b4636d4bfc729
Frost-Phoenix <67cyril6767@gmail.com>
2024-06-29 22:11:39 +0200

    read file

--- src/main.c
+++ src/main.c
@@ -1,10 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 
-int main(void) {
+#define TRUE 1
+#define FALSE 0
 
-    puts("Brainfuck");
+#define MEMORY_NB_BYTES 30000
+
+
+static int _valid_char(char c) {
+    return c == '+' || c == '-' || c == '<' || c == '>' || c == '[' || c == ']' || c == '.' || c == ',';
+}
+
+int main(int argc, char* argv []) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file_name>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    uint8_t memory[MEMORY_NB_BYTES] = { 0 };
+    uint8_t* p = memory;
+
+    FILE* fp;
+    int c;
+
+    fp = fopen(argv[1], "rt");
+    if (fp == NULL) {
+        perror(argv[1]);
+        exit(1);
+    }
+
+    while ((c = fgetc(fp)) != EOF) {
+        if (!_valid_char(c)) {
+            continue;
+        }
+
+        switch (c) {
+            case '+':
+                (*p)++;
+                break;
+            case '-':
+                (*p)--;
+                break;
+            case '>':
+                p++;
+                break;
+            case '<':
+                (*p)--;
+                break;
+
+        }
+    }
+
+    printf("\n");
+    fclose(fp);
 
     return EXIT_SUCCESS;
 }
